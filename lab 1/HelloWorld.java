c3de98d01db1582e0d0f50900cf757d2cd7a6810
EndouKoumei <danghongminh365@gmail.com>
2024-09-25 21:57:33 +0700

    Editted

--- lab 1/HelloWorld.java
+++ lab 1/HelloWorld.java
@@ -2,6 +2,8 @@
 //Example 1: HelloWorld.java
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello, world");
+		System.out.println("Dang Hong Minh - 20225740");
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world");
 	}
 }
