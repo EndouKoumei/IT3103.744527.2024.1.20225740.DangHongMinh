8482935e8b20077c9b5bed88a896d67263a282c6
EndouKoumei <danghongminh365@gmail.com>
2024-09-27 00:19:34 +0700

    EDITTED

--- lab 1/FirstDegreeEquation.java
+++ lab 1/FirstDegreeEquation.java
@@ -1,20 +1,21 @@
+//Dang Hong Minh - 20225740
 import javax.swing.JOptionPane;
 public class FirstDegreeEquation {
     public static void main(String[] args) {
         String strNotification = "The equation is: ";
-        // Get value 'a' from the user
+        //Get value 'a' from the user
         String inputA = JOptionPane.showInputDialog(null, "Enter the value a:", "Input a",
         JOptionPane.INFORMATION_MESSAGE);
         double a = Double.parseDouble(inputA);
         strNotification += a + "x + ";
-        // Get value 'b' from the user
+        //Get value 'b' from the user
         String inputB = JOptionPane.showInputDialog(null, "Enter the value b:", "Input b",
         JOptionPane.INFORMATION_MESSAGE);
         double b = Double.parseDouble(inputB);
         strNotification += b + " = 0";
-        // Check if 'a' is zero
-        if (a == 0) { // Special cases when a is zero
-            if (b == 0) { // Special cases when b is zero
+        //Check if 'a' is zero
+        if (a == 0) { //Special cases when a is zero
+            if (b == 0) { //Special cases when b is zero
                 JOptionPane.showMessageDialog(null, strNotification, "Your input", JOptionPane.INFORMATION_MESSAGE);
                 JOptionPane.showMessageDialog(null, "The equation has infinite solutions.", "Infinite Solution!", JOptionPane.INFORMATION_MESSAGE);
             } else {
@@ -22,7 +23,7 @@ public class FirstDegreeEquation {
                 JOptionPane.showMessageDialog(null, "The equation has no solution.", "No Solution!", JOptionPane.INFORMATION_MESSAGE);
             }
         } else {
-            // Solve the equation
+            //Solve the equation
             double solution = -b / a;
             JOptionPane.showMessageDialog(null, strNotification, "Your input", JOptionPane.INFORMATION_MESSAGE);
             JOptionPane.showMessageDialog(null, "The solution is x = " + solution, "Solution", JOptionPane.INFORMATION_MESSAGE);
