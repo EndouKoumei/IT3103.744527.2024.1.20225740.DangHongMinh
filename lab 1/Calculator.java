4544d9daaa8a6c4973e0b5cce81c1168a70a623a
EndouKoumei <danghongminh365@gmail.com>
2024-09-26 23:44:19 +0700

    editted

--- lab 1/Calculator.java
+++ lab 1/Calculator.java
@@ -3,13 +3,12 @@
 import javax.swing.JOptionPane;
 public class Calculator {
     public static void main(String[] args) {
-        String strNum1, strNum2;
         String strNotification = "You've just entered: ";
-        strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number",
+        String strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number",
         JOptionPane.INFORMATION_MESSAGE);
         double num1 = Double.parseDouble(strNum1);
         strNotification += strNum1 + " and ";
-        strNum2 = JOptionPane.showInputDialog(null, "Please input the second number: ", "Input the second number",
+        String strNum2 = JOptionPane.showInputDialog(null, "Please input the second number: ", "Input the second number",
         JOptionPane.INFORMATION_MESSAGE);
         double num2 = Double.parseDouble(strNum2);
         strNotification += strNum2;
---
+++ lab 1/FirstDegreeEquation.java
@@ -0,0 +1,31 @@
+import javax.swing.JOptionPane;
+public class FirstDegreeEquation {
+    public static void main(String[] args) {
+        String strNotification = "The equation is: ";
+        // Get value 'a' from the user
+        String inputA = JOptionPane.showInputDialog(null, "Enter the value a:", "Input a",
+        JOptionPane.INFORMATION_MESSAGE);
+        double a = Double.parseDouble(inputA);
+        strNotification += a + "x + ";
+        // Get value 'b' from the user
+        String inputB = JOptionPane.showInputDialog(null, "Enter the value b:", "Input b",
+        JOptionPane.INFORMATION_MESSAGE);
+        double b = Double.parseDouble(inputB);
+        strNotification += b + " = 0";
+        // Check if 'a' is zero
+        if (a == 0) { // Special cases when a is zero
+            if (b == 0) { // Special cases when b is zero
+                JOptionPane.showMessageDialog(null, strNotification, "Your input", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, "The equation has infinite solutions.", "Infinite Solution!", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, strNotification, "Your input", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, "The equation has no solution.", "No Solution!", JOptionPane.INFORMATION_MESSAGE);
+            }
+        } else {
+            // Solve the equation
+            double solution = -b / a;
+            JOptionPane.showMessageDialog(null, strNotification, "Your input", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "The solution is x = " + solution, "Solution", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
