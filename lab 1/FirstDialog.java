be77de86c5ccd8f4fec5859469256167b684f368
EndouKoumei <danghongminh365@gmail.com>
2024-09-26 20:25:13 +0700

    Editted

---
+++ lab 1/FirstDialog.java
@@ -0,0 +1,9 @@
+//Dang Hong Minh - 20225740
+//Example 2: First Dialog.java
+import javax.swing.JOptionPane;
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
