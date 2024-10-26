96fbaefa8ad92bf16c03c3e3b6ac97b3d8fe38b6
EndouKoumei <danghongminh365@gmail.com>
2024-10-26 20:15:31 +0700

    update

--- lab2/Aims.java
+++ lab2/Aims.java
@@ -7,15 +7,20 @@ public class Aims {
         // Create new cart
         Cart cart = new Cart();
         // Create new DVD object and add to cart
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc
+		("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
 		cart.addDigitalVideoDisc(dvd1);
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Sci-Fi", "George Lucas", 24.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc
+		("Star Wars", "Sci-Fi", "George Lucas", 24.95f);
 		cart.addDigitalVideoDisc(dvd2);
-		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", 18.99f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc
+		("Aladin", 18.99f);
 		cart.addDigitalVideoDisc(dvd3);
-        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Avatar", "Sci-Fi", 32.99f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc
+		("Avatar", "Sci-Fi", 32.99f);
         cart.addDigitalVideoDisc(dvd4);
-        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Fate/Stay Night", "Animation", 12.99f);
+        DigitalVideoDisc dvd5 = new DigitalVideoDisc
+		("Fate/Stay Night", "Animation", 12.99f);
         cart.addDigitalVideoDisc(dvd5);
        	//Test remove
 		cart.removeDigitalVideoDisc(dvd2);
