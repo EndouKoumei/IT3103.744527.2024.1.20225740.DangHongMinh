6708fbdcddd8c209c7d79f5572d3643398c1c1e5
EndouKoumei <163804188+EndouKoumei@users.noreply.github.com>
2024-10-27 00:54:31 +0700

    Update DigitalVideoDisc.java

--- lab2/DigitalVideoDisc.java
+++ lab2/DigitalVideoDisc.java
@@ -7,54 +7,54 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
-    //Construct by title
+    	//Construct by title
 	public DigitalVideoDisc(String title) {
-        super();
+		super();
 		this.title = title;
 	}
-    //Construct by title, cost
+    	//Construct by title, cost
 	public DigitalVideoDisc(String title, float cost) {
-        super();
+        	super();
 		this.title = title;
-        this.cost = cost;
+        	this.cost = cost;
 	}
-    //Construct by title, category, cost 
+	//Construct by title, category, cost 
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
-        this.title = title;
+        	this.title = title;
 		this.category = category;
 		this.cost = cost;
 	}
-    //Construct by title, category, director, cost 
-    public DigitalVideoDisc(String title, String category, String director, float cost){
+    	//Construct by title, category, director, cost 
+    	public DigitalVideoDisc(String title, String category, String director, float cost){
 		super();
 		this.title = title;
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
 	}
-    ////Construct by title, category, director, length, cost
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        super();
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
-        this.director = director;
-        this.length = length;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public String getCategory() {
-        return category;
-    }
-    public float getCost() {
-        return cost;
-    }
-    public String getDirector() {
-        return director;
-    }
-    public int getLength() {
-        return length;
-    }
+	//Construct by title, category, director, length, cost
+    	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        	super();
+        	this.title = title;
+        	this.category = category;
+        	this.cost = cost;
+        	this.director = director;
+        	this.length = length;
+    	}
+    	public String getTitle() {
+        	return title;
+    	}
+    	public String getCategory() {
+        	return category;
+    	}
+    	public float getCost() {
+        	return cost;
+    	}
+    	public String getDirector() {
+        	return director;
+    	}
+    	public int getLength() {
+        	return length;
+    	}
 }
