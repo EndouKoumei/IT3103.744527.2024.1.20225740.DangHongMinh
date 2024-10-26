0d0e55cecd84ba0ff681e3f897a1f13e099e9628
EndouKoumei <danghongminh365@gmail.com>
2024-10-26 20:10:30 +0700

    Update Cart.java

--- lab2/Cart.java
+++ lab2/Cart.java
@@ -3,34 +3,34 @@ package lab2;
 public class Cart {
     public static final int MAX_ITEMS = 20;
     //Create cart
-    private DigitalVideoDisc items[] = new DigitalVideoDisc[MAX_ITEMS];
+    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_ITEMS];
     //Current number of DVD
-    private int quantity = 0;
+    private int qtyOrdered = 0;
     //Add DVD to cart
     public void addDigitalVideoDisc(DigitalVideoDisc dvd) {
         //If cart not full
-        if (quantity < MAX_ITEMS) {
-            items[quantity] = dvd;
-            quantity++;
+        if (qtyOrdered < MAX_ITEMS) {
+            itemOrdered[qtyOrdered] = dvd;
+            qtyOrdered++;
             System.out.println(dvd.getTitle()+" has been added!");
-            if (MAX_ITEMS - quantity == 1)
+            if (MAX_ITEMS - qtyOrdered == 1)
                 System.out.println("Warning! The cart is almost full.");
         } else {
             System.out.println("The cart is full! Cannot add more DVDs.");
         }
     }
     public void removeDigitalVideoDisc(DigitalVideoDisc dvd) {
-        if(items[0] ==  null) {
+        if(itemOrdered[0] ==  null) {
             System.out.println("ERROR! Your cart is empty!");
             return;
         }
-        for (int i = 0; i < quantity; i++) {
-            if (items[i].equals(dvd)) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemOrdered[i].equals(dvd)) {
                 //Remove the disc
-                for (int j = i; j < quantity - 1; j++)
-                    items[j] = items[j + 1];
-                items[quantity - 1] = null;
-                quantity--;
+                for (int j = i; j < qtyOrdered - 1; j++)
+                    itemOrdered[j] = itemOrdered[j + 1];
+                itemOrdered[qtyOrdered - 1] = null;
+                qtyOrdered--;
                 System.out.println(dvd.getTitle() + " has been removed from cart!");
                 return;
             }
@@ -40,8 +40,8 @@ public class Cart {
     }
     public float totalCost() {
         float totalCost = 0;
-        for (int i = 0; i < quantity; i++) {
-            totalCost += items[i].getCost();
+        for (int i = 0; i < qtyOrdered; i++) {
+            totalCost += itemOrdered[i].getCost();
         }
         return totalCost;
     }
