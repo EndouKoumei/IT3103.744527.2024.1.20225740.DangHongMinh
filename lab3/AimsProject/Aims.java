71fa2f4fb6297639601d52c8d8d1068db24ef8c8
EndouKoumei <danghongminh365@gmail.com>
2024-11-20 14:35:47 +0700

    Sap xep lai

--- lab3/AimsProject/Aims.java
+++ lab3/AimsProject/Aims.java
@@ -1,5 +1,5 @@
 //Dang Hong Minh - 20225740
-package lab2.AimsProject;
+package lab3.AimsProject;
 public class Aims {
     public static void main(String[] args) {
         System.out.println("\nAIMS Store Project:");
