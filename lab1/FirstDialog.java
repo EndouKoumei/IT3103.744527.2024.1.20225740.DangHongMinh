03573244ce20003971b43c76e35e6d4e652f88ee
EndouKoumei <163804188+EndouKoumei@users.noreply.github.com>
2024-11-27 12:53:39 +0700

    Delete lab1 directory

--- lab1/FirstDialog.java
+++
@@ -1,9 +0,0 @@
-//Dang Hong Minh - 20225740
-//Example 2: FirstDialog.java
-import javax.swing.JOptionPane;
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
-        System.exit(0);
-    }
-}
--- lab1/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-//Dang Hong Minh - 20225740
-//Example 1: HelloWorld.java
-public class HelloWorld {
-	public static void main (String[] args) {
-		System.out.println("Dang Hong Minh - 20225740");
-		System.out.println("Xin chao \n cac ban!");
-		System.out.println("Hello \t world");
-	}
-}
